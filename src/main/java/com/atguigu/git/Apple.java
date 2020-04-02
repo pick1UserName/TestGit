cda189d56dff06045fed2a8ee1c02f10278528e0
shitou <shitou.atguigu.com>
2020-04-02 11:31:41 +0800

    my first commit

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+/**
+*
+*@author:jt   
+*@date:2020年4月2日  上午11:27:14
+*/
+
+public class Apple {
+	
+	public static void main(String[] args) {
+		System.out.println("I'm red");
+	}
+}
---
+++ src/test/java/com/atguigu/git/Pear.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+/**
+*
+*@author:jt   
+*@date:2020年4月2日  上午11:29:13
+*/
+
+public class Pear {
+	public static void main(String[] args) {
+		System.out.println("what is this");
+	}
+}
